8d1ad005f89ef3a3b6fd94631ccff2e2335dd81a
AfazPaul1 <afazpaul@gmail.com>
2024-06-05 16:14:00 +0530

    added animal text to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import {useState} from 'react'
+
+const animals=["dog", "cat", "cow", "pig", "horse"]
+
+function randomAnimals() {
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
 
 
-const [count, setCount] = useState(0)
-const buttonClicked = (e) => {
-    //  console.log(`button clicked`);
-    setCount(count+1)
+// const [count, setCount] = useState(0)
+// const buttonClicked = (e) => {
+//     //  console.log(`button clicked`);
+//     setCount(count+1)
+// }
+// console.log(useState(6) )
+
+//array of animals
+
+
+const [animals, setAnimals] = useState([])
+
+const buttonClicked = () => {
+    setAnimals([...animals, randomAnimals()])
 }
-console.log(useState(6) )
+
+
     return (
         <div>
             <button type="button" onClick={buttonClicked}>Add Animal</button>
-            <p>{count}</p>
+            <p>{animals}</p>
+            {/* <p>{count}</p> */}
         </div>
     )
 
